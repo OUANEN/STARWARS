547e3e32c8058556f5a332f5f410333e8d79f6fa
ouanen <ouanen13@gmail.com>
2023-04-03 02:32:48 +0100

    manage unsub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Character } from './model/character.model';
 import { CharacterService } from './character.service';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   characters: Character[] = [];
-
+  characterSubscription: Subscription = new Subscription;
   constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
-    this.characterService.getCharacters().subscribe((response:any) => {
+   this.characterSubscription = this.characterService.getCharacters().subscribe((response:any) => {
       console.log(response)
       this.characters = response['results'];
     });
   }
+
+  ngOnDestroy() {
+    this.characterSubscription.unsubscribe()
+}
 }
